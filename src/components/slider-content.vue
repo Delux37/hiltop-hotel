86b87502655f79b94e161d9329abec66a51c72fe
george77 <just.gio@mail.ru>
2021-01-24 12:07:40 +0400

    finished header section with static values

--- src/components/slider-content.vue
+++ src/components/slider-content.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="slider-text-box">
-        TEST
+        <h1>Slider title</h1>
+        <p>Lorem impsum lbalbal <br> dummy tesdsajdkajdkajk</p>
     </div>
 </template>
 
@@ -17,4 +18,14 @@ export default {
     margin-left: 50px;
     border: 2px solid green;
 }
+h1{
+    font-size: 50px;
+    color: #fff;
+    margin-bottom: 22px;
+}
+p {
+    font-size: 30px;
+    color: #fff;
+}
+
 </style>
