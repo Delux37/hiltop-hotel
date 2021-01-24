21a9ce8dc07c1959a3bd063de9f95be219b606c1
george77 <just.gio@mail.ru>
2021-01-24 09:27:43 +0400

    added basic setups for css

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,17 @@
 </template>
 
 <style>
-
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+html{
+    background-color: #FFF;
+    color: #555;
+    font-family: 'Lato',  'Arial', sans-serif;
+    font-weight: 300;
+    font-size: 50px;
+    text-rendering: optimizeLegibility;
+}
 </style>
